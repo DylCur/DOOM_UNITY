f1081fd8d3904d1145ca67557b78e6176d020525
DylCur <126205426+DylCur@users.noreply.github.com>
2023-06-15 20:12:29 +0100

    Commit #2

--- Assets/Scripts/Player/playerController.cs
+++ Assets/Scripts/Player/playerController.cs
@@ -1,38 +1,59 @@
 using UnityEngine;
 
+
+
+[RequireComponent(typeof(CharacterController))]
 public class playerController : MonoBehaviour
 {
-    public float movementSpeed = 5f;
-    public float mouseSensitivity = 2f;
 
-    private float verticalRotation = 0f;
-    private CharacterController characterController;
 
-    private void Start()
-    {
-        characterController = GetComponent<CharacterController>();
-        Cursor.lockState = CursorLockMode.Locked;
+
+public float movementSpeed = 5f;
+public float mouseSensitivity = 2f;
+
+private float verticalRotation = 0f;
+private CharacterController characterController;
+public Animator animator;
+
+
+
+private void Start()
+{
+    characterController = GetComponent<CharacterController>();
+    Cursor.lockState = CursorLockMode.Locked;
+}
+
+private void Update()
+{
+    // Handle player movement
+    float horizontalMovement = Input.GetAxis("Horizontal");
+    float verticalMovement = Input.GetAxis("Vertical");
+
+    Vector3 movement = transform.right * horizontalMovement + transform.forward * verticalMovement;
+    movement.Normalize();
+    characterController.SimpleMove(movement * movementSpeed);
+
+    // Handle walking animation
+    bool isWalking = Mathf.Abs(horizontalMovement) > 0f || Mathf.Abs(verticalMovement) > 0f;
+    animator.SetBool("isWalking", isWalking);
+    isWalking = false;
+
+    if(isWalking){
+        Debug.Log(isWalking);
+    }
+    else{
+        Debug.Log("You arent walking");
     }
 
-    private void Update()
-    {
-        // Handle player movement
-        float horizontalMovement = Input.GetAxis("Horizontal");
-        float verticalMovement = Input.GetAxis("Vertical");
+    // Handle mouse camera movement
+    float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
+    float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
 
-        Vector3 movement = transform.right * horizontalMovement + transform.forward * verticalMovement;
-        movement.Normalize();
-        characterController.SimpleMove(movement * movementSpeed);
+    transform.Rotate(0f, mouseX, 0f); // Horizontal rotation
 
-        // Handle mouse camera movement
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
+    verticalRotation -= mouseY;
+    verticalRotation = Mathf.Clamp(verticalRotation, -90f, 90f); // Limit vertical rotation
 
-        transform.Rotate(0f, mouseX, 0f); // Horizontal rotation
-
-        verticalRotation -= mouseY;
-        verticalRotation = Mathf.Clamp(verticalRotation, -90f, 90f); // Limit vertical rotation
-
-        Camera.main.transform.localRotation = Quaternion.Euler(verticalRotation, 0f, 0f); // Vertical rotation
-    }
+    Camera.main.transform.localRotation = Quaternion.Euler(verticalRotation, 0f, 0f); // Vertical rotation
+}
 }
